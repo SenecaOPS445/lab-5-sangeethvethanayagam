cdbaefb16fa8e24b4fe272ebca4db4539dac513f
Sangeeth Vethanayagam <svethanayagam3@myseneca.ca>
2025-02-14 00:10:43 -0500

    implemented read_file_list function
    implemented read_file_list function

--- lab5a.py
+++ lab5a.py
@@ -4,15 +4,23 @@
 def read_file_string(file_name):
     # Takes file_name as string for a file name, returns its entire contents as a string
     f = open('data.txt', 'r')
-    return f.read()
+    file_content =  f.read()
+    f.close()
+    return file_content
 
 
-#def read_file_list(file_name):
+
+def read_file_list(file_name):
     # Takes a file_name as string for a file name, 
     # return its entire contents as a list of lines without new-line characters
+    f = open('data.txt', 'r')
+    list = f.readlines()
+    list = [line.strip() for line in list]
+    f.close()
+    return list
 
 if __name__ == '__main__':
 
     file_name = 'data.txt'
     print(read_file_string(file_name))
-    #print(read_file_list(file_name))
+    print(read_file_list(file_name))
